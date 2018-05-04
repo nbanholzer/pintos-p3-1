34ce0a494034617287ea39e4692fe62f0daa0f4b
Aaron Roberts <lordgankoo@gmail.com>
2018-05-03 21:50:08 -0600

    reworked system calls to correctly utilize page fault handler

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -50,40 +50,46 @@ syscall_init (void)
 
 // User memory access functions
 
-/* Returns true if UADDR is a valid, mapped user address,
-   false otherwise. */
-static bool verify_user (const void *uaddr) {
-  struct thread *t = thread_current ();
-  struct s_page_entry temp_spe;
-  struct hash_elem *e;
-  //printf("uaddr: %p\n", uaddr);
-  temp_spe.addr = (void*)ROUND_DOWN((unsigned)uaddr, (unsigned)PGSIZE);
-  e = hash_find(&t->s_page_table, &temp_spe.hash_elem);
-  //printf("rounded: %p\n", temp_spe.addr);
-  bool check;
-  return (uaddr < PHYS_BASE && uaddr > _f->cs &&
-    (e != NULL || ((uaddr >= _f->esp - 32) && (uaddr > PHYS_BASE-(1024*1024*8)))));
-  //TODO: may have to add reasonabless check here
+/* Reads a byte at user virtual address UADDR.
+  UADDR must be below PHYS_BASE.
+  Returns the byte value if successful, false if a segfault
+  occurred. 
+  NOTE: Only using this to check for bad pointers, not to get data. */
+static inline bool verify_user (const uint8_t *uaddr)
+{
+  if(uaddr >= (uint8_t*)PHYS_BASE)
+    return false;
+
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:"
+    : "=&a" (result) : "m" (*uaddr));
+  return result != -1;
 }
 
 /* Copies a byte from user address USRC to kernel address DST.
    USRC must be below PHYS_BASE.
    Returns true if successful, false if a segfault occurred. */
 static inline bool get_user (uint8_t *dst, const uint8_t *usrc) {
+  if(usrc >= (uint8_t*)PHYS_BASE)
+    return false;
+
   int eax;
   asm ("movl $1f, %%eax; movb %2, %%al; movb %%al, %0; 1:"
        : "=m" (*dst), "=&a" (eax) : "m" (*usrc));
-  return eax != 0;
+  return eax != -1;
 }
 
 /* Writes BYTE to user address UDST.
    UDST must be below PHYS_BASE.
    Returns true if successful, false if a segfault occurred. */
 static inline bool put_user (uint8_t *udst, uint8_t byte) {
+  if(udst >= (uint8_t*)PHYS_BASE)
+    return false;
+
   int eax;
   asm ("movl $1f, %%eax; movb %b2, %0; 1:"
        : "=m" (*udst), "=&a" (eax) : "q" (byte));
-  return eax != 0;
+  return eax != -1;
 }
 
 // wrapper around user memory access functions
@@ -99,12 +105,12 @@ static bool access_user_data
   {
     switch (uat){
       case USER_READ:
-        if(!(verify_user(src_byte+i) && get_user(dst_byte+i, src_byte+i)))
+        if(!get_user(dst_byte+i, src_byte+i))
           return false;
         break;
 
       case USER_WRITE:
-        if(!(verify_user(dst_byte+i) && put_user(dst_byte+i, *(src_byte+i))))
+        if(!put_user(dst_byte+i, *(src_byte+i)))
           return false;
         break;
 
@@ -113,18 +119,6 @@ static bool access_user_data
     }
   }
   return true;
-}
-
-// verifies that all bytes of a string pointed
-// to by S are in valid user address space
-static bool verify_string(const char* s)
-{
-  for(int i = 0; verify_user(s+i); i++)
-  {
-    if(s[i] == '\0')
-      return true;
-  }
-  return false;
 }
 
 // System calls
@@ -196,9 +190,6 @@ static int sys_write (int arg0, int arg1, int arg2)
   int bytes_written = 0;
   char *kernel_buffer;
 
-  if(buffer == NULL || !verify_user(buffer))
-    sys_exit(-1, 0, 0);
-
   // writing to stdout
   if(fd == 1)
   {
@@ -233,8 +224,8 @@ static int sys_halt(int arg0 UNUSED, int arg1 UNUSED, int arg2 UNUSED)
 static int sys_exec (int arg0, int arg1 UNUSED, int arg2 UNUSED)
 {
   const char *args = (const char*)arg0;
-
-  if(!verify_string(args))
+  
+  if(!verify_user((const unsigned char*)args))
     sys_exit(-1, 0, 0);
 
   return process_execute(args);
@@ -253,7 +244,7 @@ static int sys_create (int arg0, int arg1, int arg2 UNUSED)
   unsigned initial_size = (unsigned)arg1;
   bool success = false;
 
-  if(file == NULL || !verify_string(file))
+  if(!file || !verify_user((const unsigned char*)file))
     sys_exit(-1, 0, 0);
 
   lock_acquire(&filesys_lock);
@@ -268,7 +259,7 @@ static int sys_remove (int arg0, int arg1 UNUSED, int arg2 UNUSED)
   const char *file = (const char *)arg0;
   bool success = false;
 
-  if(file == NULL || !verify_string(file))
+  if(!file || !verify_user((const unsigned char*)file))
     sys_exit(-1, 0, 0);
 
   lock_acquire(&filesys_lock);
@@ -276,21 +267,22 @@ static int sys_remove (int arg0, int arg1 UNUSED, int arg2 UNUSED)
   lock_release(&filesys_lock);
 
   return success;
-  return 0;
 }
 
 static int sys_open (int arg0, int arg1 UNUSED, int arg2 UNUSED)
 {
   const char *file = (const char *)arg0;
 
-  if(file == NULL || !verify_string(file))
+  if(!file || !verify_user((const unsigned char*)file))
     sys_exit(-1, 0, 0);
 
   struct file *f;
   lock_acquire(&filesys_lock);
-  if(!(f = filesys_open(file)))
-    return -1;
+  f = filesys_open(file);
   lock_release(&filesys_lock);
+
+  if(!f)
+    return -1;
 
   struct open_file *of = malloc(sizeof(struct open_file));
   of->file = f;
@@ -316,8 +308,6 @@ static int sys_read (int arg0, int arg1, int arg2)
   unsigned length = (unsigned)arg2;
   char *kernel_buffer;
   int bytes_read = 0;
-  if(buffer == NULL || !verify_user(buffer))
-    sys_exit(-1, 0, 0);
 
   if(fd == 0)
   {
@@ -419,6 +409,7 @@ static void
 syscall_handler (struct intr_frame *f)
 {
   _f = f;
+  thread_current()->esp = _f->esp;
   int syscall_number = 0;
   int args[3] = {0,0,0};
 
