c399a5bda58f1c0a9bd8d380b038fef92e56e2f9
Aaron Roberts <aaron@searchspring.com>
2018-05-10 16:35:03 -0600

    forgot to declare that new frame index getter

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -30,6 +30,7 @@ struct frame
 void init_frame_table(void);
 void *get_frame (enum frame_flags, void* upage, struct thread *t);
 void *get_frame_multiple(enum frame_flags, size_t frame_cnt, void* upage, struct thread *t);
+struct lock *get_frame_lock(void *frame);
 void free_frame (void *frame);
 void free_frame_multiple(void *frames, size_t frame_cnt);
 void evict(struct frame *frame);
